353e2ec47ef920ed97a86d59071436925478182e
Edgar Chaput <chaput.edgar@gmail.com>
2016-02-09 14:57:14 +0100

    coded creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do
+article = Article.create(title: Faker::Name.title, content: Faker::Hipster)
+end
